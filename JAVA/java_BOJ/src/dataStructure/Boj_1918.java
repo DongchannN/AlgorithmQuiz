abbb52577aeb7c8825170e2e17944af49b3a511b
dongchan <ldc991104@gmail.com>
2022-05-26 14:13:56 +0900

    Boj_1918 done.

--- JAVA/java_BOJ/src/dataStructure/Boj_1918.java
+++ JAVA/java_BOJ/src/dataStructure/Boj_1918.java
@@ -7,51 +7,61 @@ import java.util.*;
 public class Boj_1918 {
     public void solution() throws IOException, EmptyStackException {
         Scanner scanner = new Scanner(System.in);
-        List<String> result = new ArrayList<String>();
-        List<Integer> priorityList = new ArrayList<>();
-        Stack stack = new Stack();
-
+        Stack<Character> operatorStack = new Stack<>();
+        ArrayList outputExpression = new ArrayList<>();
         String inputExpression = scanner.nextLine();
 
         for (int i = 0; i < inputExpression.length(); i++) {
+            char indexChar = inputExpression.charAt(i);
 
-            int priority = numbering(inputExpression.charAt(i));
-            priorityList.add(priority);
+            switch (indexChar) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    while (!(operatorStack.isEmpty()) && numberingPriority(operatorStack.peek()) >= numberingPriority(indexChar)) {
+                        outputExpression.add(operatorStack.pop());
+                    }
+                    operatorStack.add(indexChar);
+                    break;
+                case ')':
+                    while(!(operatorStack.isEmpty()) && operatorStack.peek() != '(') {
+                        outputExpression.add(operatorStack.pop());
+                    }
 
-            if (priority == 2 && priorityList.contains(1)) {
-                while (!(stack.isEmpty())) {
-                    result.add(String.valueOf(stack.pop()));
-                }
-                stack.push(inputExpression.charAt(i));
-                continue;
-            }else if (priority == 2) stack.push(inputExpression.charAt(i));
-            if (priority == 1) {
-                stack.push(inputExpression.charAt(i));
-                continue;
+                    operatorStack.pop();
+
+                    break;
+                case '(':
+                    operatorStack.push(indexChar);
+                    break;
+                default:
+                    outputExpression.add(indexChar);
             }
-            if (priority == 0) {
-                result.add(String.valueOf(stack.pop()));
-                continue;
-            }
-            if (priority == 3) {
-                result.add(String.valueOf(inputExpression.charAt(i)));
-            }
+
         }
-        while (!(stack.isEmpty())) result.add(String.valueOf(stack.pop()));
-        String ans = String.join("",result);
+
+        while(!(operatorStack.isEmpty())) {
+                outputExpression.add(operatorStack.pop());
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i<outputExpression.size(); i++){
+            ans.append(outputExpression.get(i));
+        }
         System.out.println(ans);
-
     }
-    int numbering(char operator) {
+
+    int numberingPriority(char operator) {
         if (operator == '+' || operator == '-') {
-            return 2;
-        }
-        else if (operator == '*' || operator == '/') {
             return 1;
         }
-        else if (operator == ')') {
+        else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+        else if (operator == ')' || operator == '(') {
             return 0;
         }
-        else return 3;
+        return -1;
     }
 }
