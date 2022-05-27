6572547944688f22262f4c0bd0c66a737133a0e7
dongchan <ldc991104@gmail.com>
2022-05-27 23:58:43 +0900

    add Boj_1743 file.

---
+++ JAVA/java_BOJ/src/dataStructure/Boj_1743.java
@@ -0,0 +1,11 @@
+package dataStructure;
+
+import java.util.Scanner;
+
+public class Boj_1743 {
+    public void solution() {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str.length());
+    }
+}
