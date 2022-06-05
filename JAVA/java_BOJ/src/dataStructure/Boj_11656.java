8d9a8915763a6200c37cd9e1efd0f1d73501893d
dongchan <ldc991104@gmail.com>
2022-06-06 01:30:35 +0900

    Boj_11656. Sort alphabetically. done.

--- JAVA/java_BOJ/src/dataStructure/Boj_11656.java
+++ JAVA/java_BOJ/src/dataStructure/Boj_11656.java
@@ -21,43 +21,55 @@ public class Boj_11656 {
 
     }
     private void SortingWord(String[] word) {
+        // 1). 비교하는 단어가 비교되는 단어의 앞 부분과 일치하는지 비교   EX) 일치하는 경우 예 : EXAM , EXAMPLE
+        // 2). 1.에 해당되면 짧은 문자를 앞으로 정렬.
+        // 3). 1.에 해당되지 않으면 첫번째 자리부터 문자를 비교. 문자가 같으면 그 다음 문자 비교해서 정렬.
+
+
+
         int arrLength = word.length;             //배열의 크기 = arrLength
 
+        //word[0]를 word[i]와 i++하며 비교 후 자리 옮기기
         for (int i = 0; i < arrLength; i++) {
             for (int j = 0; j < arrLength - 1; j++) {
 
+                //1). 비교하는 문자가 비교되는 문자보다 더 길며 1.에 해당되는 경우 자리 바꾸기 // 바꾼후 continue.
+                if (word[j].length() > word[j + 1].length() && Objects.equals(word[j].substring(0, word[j+1].length()), word[j+1])) {
+                    String saver = word[j];
+                    word[j] = word[j + 1];
+                    word[j + 1] = saver;
+                    continue;
+                }
+
+                //3). 어느 부분에서 최초로 같은자리의 문자가 다른지 검사.  //errorCheck 값을 통한 에러 검사
+                int index = 0;
+                int errorCheck = 0; //StringIndexOutOfBoundException 검사 에러가 나는 경우 errorCheck에 1값을 넣어 다음 if문 진입 방해.
+                while (true) {
+                    if( index >= word[j].length() || index >= word[j + 1].length()) {
+                        errorCheck = 1;
+                        index -= 1;
+                        break;
+                    }
+                    else if( word[j].charAt(index) == word[j + 1].charAt(index)) {
+                        index += 1;
+
+                    }
+
+                    else if( word[j].charAt(index) != word[j + 1].charAt(index)) {
+
+                        break;
+                    }
+
+
+
+                }
                 //앞자리에 따른 단어 분류
-                if((int) word[j].charAt(0) > (int) word[j+1].charAt(0)) {
+                if(errorCheck != 1 && (int) word[j].charAt(index) > (int) word[j+1].charAt(index)) {     //StringIndexOutOfBoundException.
                     String saver = word[j];
                     word[j] = word[j+1];
                     word[j+1] = saver;
                 }
 
-                //앞자리가 같을 시
-                else if((int) word[j].charAt(0) == (int) word[j+1].charAt(0)) {
-
-                    //"word[j]의 길이 > word[j+1]의 길이" 이며 짧은 단어가 긴단어 앞부분에 포함 될 시 짧은 단어를 앞으로  EX) ON, ONLY (ON이 앞부분에 포함)
-                    if ( word[j].length() > word[j+1].length()  &&  word[j+1] == word[j].substring(0, word[j+1].length() + 1) ){ // Objects.equals(word[j+1], word[j].substring(0, word[j+1].length() + 1))
-                        String saver = word[j];
-                        word[j] = word[j + 1];
-                        word[j + 1] = saver;
-                    }
-
-                    else { //"word[j]의 길이 < word[j+1]의 길이"일 때 "OR word[j]의 길이 > word[j+1]의 길이"이지만 순서를 바꿀 필요가 없을 경우 EX) ORUNE, OGKDF 또는 ONLY, ON.
-                        int index = 1;  //이미 첫자리 비교했으므로 두번째 알파뱃부터 비교.
-                        //다른 글자가 나오면 index 에 저장 후 탈출. index 가 word의 인덱스를 넘어가면 break.
-                        while ((int) word[j].charAt(index) == (int) word[j+1].charAt(index)) { //StringIndexOutOfBoundException.
-                            index += 1;
-                            if( index >= word[j].length() -1 || index >= word[j + 1].length() -1) break;
-                        }
-                        //저장된 인덱스 이용해 알파뱃 우선을 비교
-                        if((int) word[j].charAt(index) > (int) word[j+1].charAt(index)) {
-                            String saver = word[j];
-                            word[j] = word[j+1];
-                            word[j+1] = saver;
-                        }
-                    }
-                }
             }
         }
         for (String s : word) {
