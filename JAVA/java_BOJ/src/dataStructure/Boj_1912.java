c0c7c0527671afce42564ba96e8425df513a1c37
dongchan <ldc991104@gmail.com>
2022-07-21 16:16:40 +0900

    Boj_1912. done.

--- JAVA/java_BOJ/src/dataStructure/Boj_1912.java
+++ JAVA/java_BOJ/src/dataStructure/Boj_1912.java
@@ -5,41 +5,28 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Boj_1912 {
+    static int[] dp;
     public void solution() {
         Scanner scanner = new Scanner(System.in);
         int arrSize = scanner.nextInt();
-
-        ArrayList<Long> sumArray = new ArrayList<Long>();
         int[] arr = new int[arrSize];
+
+        dp = new int[arrSize];
 
         for(int i = 0; i < arrSize; i++) {
             arr[i] = scanner.nextInt();
-            sumArray.add((long) arr[i]);
         }
 
-        for(int i = 1; i <= arrSize; i++) {
-            int j = arrSize + 1 - i;
-            for(int k = 0; k < j; k++) {
-                long addElement = sumPartOfArr(k, k+i-1, arr);
-                if(addElement > Collections.max(sumArray)) {
-                    sumArray.clear();
-                    sumArray.add(addElement);
-                }
-            }
-        }
-        long max = Collections.max(sumArray);
+        dp[0] = arr[0];
 
+        for(int i = 1; i < arrSize; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+        }
+
+        int max = dp[0];
+        for(int i = 1; i < arrSize; i++) {
+            if(dp[i] > max) max = dp[i];
+        }
         System.out.println(max);
-
-
     }
-
-    long sumPartOfArr(int startPoint, int endPoint, int[] arr) {
-        long sum = 0;
-        for(int i = startPoint; i <= endPoint; i++) {
-            sum += arr[i];
-        }
-        return sum;
-    }
-
 }
