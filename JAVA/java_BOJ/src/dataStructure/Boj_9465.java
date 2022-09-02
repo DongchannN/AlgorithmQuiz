30303e098aa923b78acd4e167d04693e882cfece
dongchan <ldc991104@gmail.com>
2022-09-02 10:41:59 +0900

    Boj_9465. Dp[n][0] - case that upper card is last. Dp[n][1] - case that down card is last. done.

--- JAVA/java_BOJ/src/dataStructure/Boj_9465.java
+++ JAVA/java_BOJ/src/dataStructure/Boj_9465.java
@@ -1,6 +1,5 @@
 package dataStructure;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Boj_9465 {
@@ -14,7 +13,7 @@ public class Boj_9465 {
         for (int i = 0; i < testCase; i++) {
             int n = scanner.nextInt();
 
-            int[] Dp = new int[n+2];
+            int[][] Dp = new int[n+2][2];
             int[] upArr = new int[n+2];
             int[] downArr = new int[n+2];
 
@@ -26,19 +25,25 @@ public class Boj_9465 {
                 downArr[j] = scanner.nextInt();
             }
 
-            Dp[0] = Math.max(upArr[0], downArr[0]);
+            /**
+             * Dp[n][0] - 위에서 고른 카드가 마지막일 때 최댓값.
+             * Dp[n][1] - 아래에서 고른 카드가 마지막일 때 최댓값.
+             *
+             * Dp[n][0] - Dp[n-2]중 최댓값과 지금의 카드를 더한 값, Dp[n-1][1] 과 지금의 카드를 더한 값 비교.
+             * Dp[n][1] - Dp[n-2]중 최댓값과 지금의 카드를 더한 값, Dp[n-1][0] 과 지금의 카드를 더한 값 비교.
+             */
+            Dp[0][0] = upArr[0];
+            Dp[0][1] = downArr[0];
+            Dp[1][0] = upArr[1] + downArr[0];
+            Dp[1][1] = downArr[1] + upArr[0];
 
-            Dp[1] = Math.max(upArr[0] + downArr[1], downArr[0] + upArr[1]);
+
             for (int j = 2; j < n; j++) {
-                int value;
-                if (Dp[j - 1] == upArr[j - 1]) {
-                    value = downArr[j];
-                } else value = upArr[j];
-
-                Dp[j] = Math.max(Dp[j - 1] + value, Dp[j - 2] + Math.max(downArr[j], upArr[j]));
+                Dp[j][0] = Math.max(upArr[j] + Dp[j - 1][1], upArr[j] + Math.max(Dp[j - 2][0], Dp[j - 2][1]));
+                Dp[j][1] = Math.max(downArr[j] + Dp[j - 1][0], downArr[j] + Math.max(Dp[j - 2][0], Dp[j - 2][1]));
             }
 
-            ans[i] = Dp[n-1];
+            ans[i] = Math.max(Dp[n-1][0], Dp[n-1][1]);
         }
 
         for (int i = 0; i < ans.length; i++) {
