a967e911973c14cb495f27c37a640d44e1f765ee
DongchannN <ldc991104@gmail.com>
2023-11-20 15:08:47 +0900

    Boj Algorithm 7785 Set을 이용한 시간 초과 방지

---
+++ JAVA/java_BOJ/src/sort/Boj_7785.java
@@ -0,0 +1,24 @@
+package sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_7785 { // Set 이용
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            if (Objects.equals(s[1], "enter")) set.add(s[0]);
+            else set.remove(s[0]);
+        }
+        Object[] objects = set.toArray();
+        Arrays.sort(objects, (a, b) -> -a.toString().compareTo(b.toString()));
+        for (int i = 0; i < objects.length; i++)
+            System.out.println(objects[i].toString());
+    }
+}
