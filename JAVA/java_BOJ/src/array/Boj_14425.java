e330e4d095508ba4c3b9374d3b3793c1917a852f
DongchannN <ldc991104@gmail.com>
2023-12-07 21:30:32 +0900

    Boj Algorithm 14425

---
+++ JAVA/java_BOJ/src/array/Boj_14425.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.*;
+import java.io.*;
+
+public class Boj_14425 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        Set<String> groupS = new TreeSet<>();
+        for (int i = 0; i < n; i++)
+            groupS.add(br.readLine());
+
+        String[] groupM = new String[m];
+        for (int i = 0; i < m; i++)
+            groupM[i] = br.readLine();
+
+        int cnt = 0;
+        for (int i = 0; i < m; i++)
+            if (groupS.contains(groupM[i]))
+                cnt++;
+        System.out.println(cnt);
+    }
+}
