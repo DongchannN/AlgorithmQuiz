b98c4db003833bfd7e5bd53c81cc40f89fea96b5
DongchannN <ldc991104@gmail.com>
2024-01-04 23:55:45 +0900

    Boj 2805 solve by parametric search

--- JAVA/java_BOJ/src/solutionExecution.java
+++ JAVA/java_BOJ/src/solutionExecution.java
@@ -1,12 +1,11 @@
-import math.*;
-import string.*;
 
+import binarySearch.*;
 
 import java.io.IOException;
 
 
 public class solutionExecution {
     public static void main(String[] args) throws IOException {
-        new Boj_1157().solution();
+        new Boj_2805().solution();
     }
 }
