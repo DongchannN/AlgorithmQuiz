8cca25d74aa5c818ec9575909ccf782a765d5e07
DongchannN <ldc991104@gmail.com>
2024-03-12 21:14:50 +0900

    Boj 20500 solve.

---
+++ JAVA/java_BOJ/src/Boj_20500.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Boj_20500 {
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n + 2];
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + (2 * dp[i - 2])) % 1000000007;
+        }
+        System.out.println(dp[n]);
+    }
+}
